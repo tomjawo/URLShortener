73d92c11563306151d3a441e64dd287f3aeaf1c6
Tomasz Jaworski <tom.jawo@outlook.com>
2018-03-09 22:41:39 +0100

    Added basic model and repository

---
+++ URLShortener/Models/URL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace URLShortener.Models
+{
+    public class URL
+    {
+        public int Id { get; set; }
+        public string Address{ get; set; }
+        public string ShortAdress { get; set; }
+    }
+}
