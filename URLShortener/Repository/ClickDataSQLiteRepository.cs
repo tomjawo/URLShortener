e7ede3ef739a72328e44c93cf400c0460a589733
Tomasz Jaworski <tom.jawo@outlook.com>
2018-04-04 01:36:02 +0200

    Added ClickData repository

---
+++ URLShortener/Repository/ClickDataSQLiteRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using URLShortener.Models;
+
+namespace URLShortener.Repository
+{
+    public class ClickDataSQLiteRepository : IClickDataRepository
+    {
+        private URLDbContext _context;
+
+        public ClickDataSQLiteRepository(URLDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(ClickData click)
+        {
+
+            _context.Clicks.Add(click);
+            _context.SaveChanges();
+        }
+
+        public ClickData Get(URL url)
+        {
+            return _context.Clicks.Where(x => x.url == url).FirstOrDefault();
+        }
+
+        public IEnumerable<ClickData> GetAll()
+        {
+            return _context.Clicks.ToList();
+        }
+
+        public void Update(ClickData click)
+        {
+            _context.Clicks.Attach(click);
+            _context.Entry(click).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ URLShortener/Repository/IClickDataRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using URLShortener.Models;
+
+namespace URLShortener.Repository
+{
+    public interface IClickDataRepository
+    {
+        ClickData Get(URL url);
+        IEnumerable<ClickData> GetAll();
+        void Update(ClickData click);
+        void Create(ClickData click);
+
+    }
+}
